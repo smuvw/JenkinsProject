549aa13685527e106b59d168fdd89bafc2d85813
Administrator <Administrator@192.168.222.1>
2017-04-09 14:19:18 -0500

    moving files to rep

---
+++ com.learnautomationselenium/src/test/java/com/learnautomationselenium/DemoMaven.java
@@ -0,0 +1,20 @@
+package com.learnautomationselenium;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DemoMaven {
+	
+	@Test
+	public void Login(){
+		
+		System.out.println("Welcone to MAVEN ");
+		System.out.println(10/2);
+		System.out.println("Welcone to java ");
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		
+	}
+
+}
